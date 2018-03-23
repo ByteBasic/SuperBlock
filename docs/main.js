554dde69bcf3c4c40304d52ef46194e9ca4e74e9
Justin C <34142296+Warrior392@users.noreply.github.com>
2018-03-22 19:10:54 -0500

    Minor Improvements

--- docs/main.js
+++ docs/main.js
@@ -1,66 +1,55 @@
 var canvas = document.getElementById("Canvas");
 	ctx = canvas.getContext("2d");
+
 		var Player = {
 			color: "#DDD",
 			vx:0,
 			vy:0,
-			maxFallVel:20,
-			maxUpwardsVel:1500,
 			x:475,
 			y:225,
+			canJump:false,
 		}
+
 		var gravity = 1;
-		var canJump = false;
-		var Keys = new Array(256).fill(false)
+		var friction = 0.8;
+		var Keys = {}
+
 	function update() {
 		ctx.clearRect(0,0,1000,500);
 		ctx.fill();
 		ctx.fillStyle  = Player.color
-			if(Keys[87] == true && canJump == true) //W
-			{
-				Player.vy += 20;
-				Player.y -= Player.vy;
-				canJump = false;
-			} else {
-				Player.vy -= gravity;
-				Player.y -= Player.vy; 
-			}
-			
-			if(Keys[65] == true){ //A
-				Player.vx -= 5; 
-				Player.x += Player.vx;
-			} else {Player.vx = 0}
+			if(Keys["w"] == true && Player.canJump == true){
+				Player.vy -= 20;
+				Player.canJump = false;
+			} else {Player.vy += gravity}
 
-			if(Keys[68] == true){ //D
-				Player.vx += 5;
-				Player.x += Player.vx;
-			} else {Player.vx = 0}
+			if(Keys["a"] == true){Player.vx -= 2}
+			if(Keys["d"] == true){Player.vx += 2}
 
-			if(Player.vy > Player.maxUpwardsVel){ Player.vy = Player.maxUpwardsVel;}
 			if(Player.vy < -Player.maxFallVel){ Player.vy = -Player.maxFallVel;}
 
 				if(Player.y > 450){
 					Player.vy = 0;
 					Player.y = 450;
-					canJump = true;
+					Player.canJump = true;
 				}
+
+				Player.vx *= friction;
+
+				Player.x += Player.vx;
+				Player.y += Player.vy;
+
 		ctx.fillRect(Player.x,Player.y,50,50)
 		requestAnimationFrame(update);
 	}
+
 	update();
+	
 	function onKeyDown(event) {
-		Keys[event.keyCode] = true;
+		Keys[event.key] = true;
 	}
 	function onKeyUp(event) {
-		Keys[event.keyCode] = false;
+		Keys[event.key] = false;
 	}
 	document.addEventListener("keyup", onKeyUp);
 	document.addEventListener("keydown", onKeyDown);
-//Player
-	//Player Object: X Y VelX VelY JumpForce CanJump BCol TCol RCol LCol
-	//Key Input (W,A,S,D,Space)
-	//Player Movement
-//Tile Engine
-	//Tile Array
-	//Tile Defining: TileSize TileX TileY TileType
-	//Tile Array Reader
