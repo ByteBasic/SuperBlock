35a2c9aec95dd853f8a0e5b94e33ec57814d28d4
Justin C <34142296+Warrior392@users.noreply.github.com>
2018-03-31 17:56:42 -0500

    Tile System Implemented

--- docs/main.js
+++ docs/main.js
@@ -1,55 +1,109 @@
-var canvas = document.getElementById("Canvas");
-	ctx = canvas.getContext("2d");
+//Map Data
+var MapData = ["                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "                                        ",
+			   "F                                      F",
+			   "F                                      F",
+			   "F                                      F",
+			   "F                                      F",
+			   "F                FFFFFF                F",
+			   "F                                      F",
+			   "F                                      F",
+			   "FFFFFFFF                        FFFFFFFF",
+			   ];
 
-		var Player = {
-			color: "#DDD",
-			vx:0,
-			vy:0,
-			x:475,
-			y:225,
-			speed:1.5,
-			canJump:false,
-		}
+//Map Variables
+	var TileSize = 25;
+	var TileColors = {
+		floor: "rgba(180,180,180,255)",
+	}
+	var CurrentTile;
+	var CurrentLevel = 1;
 
-		var currentColorMode = 0;
+//Canvas Variables
+	var canvas = document.getElementById("Canvas");
+	var ctx = canvas.getContext("2d");
 
-		var gravity = 1;
-		var friction = 0.8;
-		var Keys = {}
+//General Global Variables
+	var currentColorMode = 0;
+	var Keys = {}
+
+//Entity Physics Variables
+	var gravity = 1;
+	var friction = 0.8;
+
+//Player Object
+	var Player = {
+		color: "#DDD",
+		vx:0,
+		vy:0,
+		x:488,
+		y:225,
+		speed:1.5,
+		jumpForce:15,
+		canJump:false,
+	}
+//Main Update Function
 
 	function update() {
 		ctx.clearRect(0,0,1000,500);
-		ctx.fill();
-		ctx.fillStyle  = Player.color
-			if((Keys["w"] == true || Keys[" "] == true) && Player.canJump == true){
-				Player.vy -= 15;
-				Player.canJump = false;
-			} else {Player.vy += gravity}
-				if(Keys["a"] == true){Player.vx -= Player.speed}
-				if(Keys["d"] == true){Player.vx += Player.speed}
 
-			if(Player.y > 475){
-				Player.vy = 0;
-				Player.y = 475;
-				Player.canJump = true;
+		var mapX = Math.floor(Player.x/TileSize)
+		var mapY = Math.floor(Player.y/TileSize)
+
+		if((Keys["w"] == true || Keys[" "] == true) && Player.canJump == true){
+			Player.vy -= Player.jumpForce;
+			Player.canJump = false;
+		} else {Player.vy += gravity}
+			if(Keys["a"] == true){Player.vx -= Player.speed}
+			if(Keys["d"] == true){Player.vx += Player.speed}
+		
+			Player.vx *= friction;
+			Player.x += Player.vx;
+			Player.y += Player.vy;
+
+		
+		for(var i = 0; i < 40; i++) {
+			for(var j = 0 ; j < 20; j++) {
+				 CurrentTile = MapData[j].charAt(i);
+				if(CurrentTile == "F") {
+					ctx.fillStyle = TileColors.floor
+				 	ctx.fillRect( i * TileSize, j * TileSize, TileSize, TileSize)
+				}
 			}
-				Player.vx *= friction;
-				Player.x += Player.vx;
-				Player.y += Player.vy;
+		}
+		if(Player.y > 475){
+			Player.vy = 0;
+			Player.y = 475;
+			Player.canJump = true;
+		}
 
+		ctx.fillStyle  = Player.color
 		ctx.fillRect(Player.x,Player.y,25,25)
 		requestAnimationFrame(update);
 	}
 
 	update();
 	
+//Misc. Functions
+
 	function onKeyDown(event) {
 		Keys[event.key] = true;
 	}
 	function onKeyUp(event) {
 		Keys[event.key] = false;
 	}
-	function darkMode() {
+
+	function colorMode() {
 		if(currentColorMode == 0){
 			document.body.style.backgroundColor = "rgba(100,100,100,255)"
 			document.getElementById("Title").style.color = "rgba(80,80,80,255)"
@@ -58,7 +112,10 @@ var canvas = document.getElementById("Canvas");
 				document.getElementById("ModeButton").textContent = "Light Mode"
 			document.getElementById("Canvas").style.backgroundColor = "rgba(80,80,80,255)"
 			Player.color = "rgba(100,100,100,255)"
-				currentColorMode = 1;
+
+			//Tiles
+				TileColors.floor = "rgba(60,60,60,255)"
+					currentColorMode = 1;
 		} else {
 			document.body.style.backgroundColor = "rgba(230,230,230,255)"
 			document.getElementById("Title").style.color = "rgba(200,200,200,255)"
@@ -67,8 +124,13 @@ var canvas = document.getElementById("Canvas");
 				document.getElementById("ModeButton").textContent = "Dark Mode"
 			document.getElementById("Canvas").style.backgroundColor = "rgba(255,255,255,255)"
 			Player.color = "#DDD"
-				currentColorMode = 0;
+
+			//Tiles
+				TileColors.floor = "rgba(180,180,180,255)"
+					currentColorMode = 0;
 		}
 	}
+
+//Key Event Listeners
 	document.addEventListener("keyup", onKeyUp);
 	document.addEventListener("keydown", onKeyDown);
